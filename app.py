b67043fc5626cfa8d8bc4020446808ac6beddb6e
Jacob Emory Bullin <jacobbullin@roadrunner.com>
2025-03-27 19:58:23 -0400

    removing about page, seems a little redundant considering most of the information will be on the main page

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ def home():
 def projects():
     return render_template('projects.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
